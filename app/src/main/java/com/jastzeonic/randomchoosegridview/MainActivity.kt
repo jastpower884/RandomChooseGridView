5f4b8bdae8582e32e5e103399ff9ffcc419e9174
Jast <jastpower884@gmail.com>
2019-04-14 14:54:04 +0800

    finish question 1 request.

--- app/src/main/java/com/jastzeonic/randomchoosegridview/MainActivity.kt
+++ app/src/main/java/com/jastzeonic/randomchoosegridview/MainActivity.kt
@@ -2,11 +2,41 @@ package com.jastzeonic.randomchoosegridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var column = 1
+    var row = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edit_text_1.addTextChangedListener {
+            column = getTextNumber(it.toString())
+        }
+
+        edit_text_2.addTextChangedListener {
+            row = getTextNumber(it.toString())
+        }
+    }
+
+    private fun getTextNumber(text: String): Int {
+        var textNumber = 0
+        textNumber = try {
+            text.toInt()
+        } catch (exception: NumberFormatException) {
+            1
+        }
+        return if (textNumber > 0) {
+            textNumber
+        } else {
+            1
+        }
     }
 }
